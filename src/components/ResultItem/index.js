7fe4d722352504532549775ba80d8a0d43fdb656
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-03-09 13:29:59 +0000

    add more api data

--- src/components/ResultItem/index.js
+++ src/components/ResultItem/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 export default function ResultItem({data}) {
     return (
-        <div>
+        <div className="result-item">
             {/* to-do: make pretty */}
-            {data.full_name}
+            <h3>{data.name}</h3>
+            <p>Forks: {data.forks_count}</p>
+            <p>Stargazers: {data.stargazers_count}</p>
+            <p>Open Issues: {data.open_issues}</p>
         </div>
     );
 }

5b0f152b13b0085e061b26fe5d8415f4c72946ff
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-03-09 14:19:21 +0000

    fix long repo names

--- src/components/ResultItem/index.js
+++ src/components/ResultItem/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 export default function ResultItem({data}) {
+
+    let dataName
+    if (data.name.length > 26 ) {
+        dataName = data.name.substring(0, 27) + "..."
+    } else {
+        dataName = data.name
+    }
+    
     return (
         <div className="result-item">
             {/* to-do: make pretty */}
-            <h3>{data.name}</h3>
+            <h3>{dataName}</h3>
             <p>Forks: {data.forks_count}</p>
             <p>Stargazers: {data.stargazers_count}</p>
             <p>Open Issues: {data.open_issues}</p>

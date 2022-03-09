470b818c3fc10bbb76d77688eb82b1fb23a31729
rxdvd <rxdvd0@gmail.com>
2022-03-09 15:34:18 +0000

    implement clickable repo names

--- src/components/ResultItem/index.js
+++ src/components/ResultItem/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ResultItem({data}) {
     return (
         <div role="listitem" className="result-item">
-            <h3>{data.name}</h3>
+            <h3><a href={data.html_url} target="_blank">{data.name}</a></h3>
             <p>Forks: {data.forks_count}</p>
             <p>Stargazers: {data.stargazers_count}</p>
             <p>Open Issues: {data.open_issues}</p>

7f155158642a2ec835d73ede8129c8a399b3b1d2
rxdvd <rxdvd0@gmail.com>
2022-03-09 14:58:05 +0000

    add Form tests

--- src/components/Form/Form.spec.js
+++ src/components/Form/Form.spec.js
@@ -1,13 +1,33 @@
 import { default as Form } from ".";
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 describe("Search Form", () => {
     beforeEach(() => {
         renderWithReduxProvider(<Form />);
     });
 
+    it("renders a heading 'GitHub Repository Search'", () => {
+        const heading = screen.getByRole("heading");
+        expect(heading).toBeInTheDocument();
+        expect(heading).toHaveTextContent("GitHub Repository Search");
+    });
+
     it("renders a form", () => {
-        const form = screen.getByRole("form");
+        const form = screen.getByRole("search");
         expect(form).toBeInTheDocument();
+    });
+
+    it("renders a search input", () => {
+        const input = screen.getByLabelText("Github username");
+        expect(input).toBeInTheDocument();
+        expect(input.type).toBe("search");
+        expect(input.placeholder).toBe("GitHub username");
+    });
+
+    it("renders a Search button", () => {
+        const button = screen.getByRole("button", { name: /Search/ });
+        expect(button).toBeInTheDocument();
+        expect(button.type).toBe("submit");
     });
 });

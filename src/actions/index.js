5c00d7a28461d858473415a00c8096dd5278b17a
rxdvd <rxdvd0@gmail.com>
2022-03-09 12:59:24 +0000

    error handling

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,12 @@ export const getRepos = username => {
     return async dispatch => {
         try {
             const resp = await fetch(`https://api.github.com/users/${username}/repos`)
-            const res = await resp.json()
+            const res = await resp.json();
+            if(!Array.isArray(res)) throw res;
             dispatch({
                 type: "LOAD_REPOS",
                 payload: res
-            })
+            });
         } catch (err) {
             console.warn(err.message);
             dispatch({ type: 'SET_ERROR', payload: err.message })

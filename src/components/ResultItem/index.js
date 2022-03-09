9c7a7879e450c84d9c11684bd8b78d605245ffdd
rxdvd <rxdvd0@gmail.com>
2022-03-09 15:21:47 +0000

    refactor ellipsis on long repo names to css

--- src/components/ResultItem/index.js
+++ src/components/ResultItem/index.js
@@ -1,18 +1,9 @@
 import React from 'react';
 
 export default function ResultItem({data}) {
-
-    let dataName
-    if (data.name.length > 26 ) {
-        dataName = data.name.substring(0, 27) + "..."
-    } else {
-        dataName = data.name
-    }
-    
     return (
-        <div className="result-item">
-            {/* to-do: make pretty */}
-            <h3>{dataName}</h3>
+        <div role="listitem" className="result-item">
+            <h3>{data.name}</h3>
             <p>Forks: {data.forks_count}</p>
             <p>Stargazers: {data.stargazers_count}</p>
             <p>Open Issues: {data.open_issues}</p>

f86ba434d10ecdd5c583fe0d20a2c623eb5b3112
rxdvd <rxdvd0@gmail.com>
2022-03-09 12:16:03 +0000

    fix typo

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -5,7 +5,7 @@ const reducer = (state = initState, action) => {
         case 'LOADING':
             return { ...state, username: action.payload, loading: true };
         case 'LOAD_REPOS':
-            return { ...state, result: action.payload, loading: false, error: false };
+            return { ...state, results: action.payload, loading: false, error: false };
         case 'SET_ERROR':
             return { ...state, error: action.payload, loading: false};
         default: 

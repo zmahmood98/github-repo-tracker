43c417419e5aa3389e8d214381e17f3dd8ca5895
rxdvd <rxdvd0@gmail.com>
2022-03-09 17:20:22 +0000

    add favicon to HtmlWebpackPlugin config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -27,6 +27,7 @@ const config = {
     new HtmlWebpackPlugin({
       // used to add the JavaScript code to the HTML
       template: path.join(PUBLIC_DIRECTORY, 'index.html'),
+      favicon: path.join(PUBLIC_DIRECTORY, 'favicon.ico')
     }),
   ],
   module: {
